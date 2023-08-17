347bc4544e27473d9356345ad1e86d49dc31074c
Noah <noahkoleske@icloud.com>
2023-08-16 22:26:21 -0500

    began working on api routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
+const { readFile, writeFile } = require('fs/promises')
 const app = express()
 const PORT = 333
+const dataPath = path.join(__dirname, 'data', 'db.json')
+// const { getAndRenderNotes } = require('./public/assets/js/index.js')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -14,6 +17,15 @@ app.get('/', (req, res) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
   })
+
+app.post('/api/notes', (req, res) => {
+  const content = readFile(dataPath, "utf-8")
+  const note = JSON.parse(content)
+  writeFile(dataPath, JSON.stringify(note))
+  console.log(note)
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Express listening on http://localhost:${PORT}`)
